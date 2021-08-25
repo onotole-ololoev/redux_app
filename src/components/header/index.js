8abd45e1d327b121d3c4a18227a53d1a2dddbd28
onotole-ololoev <tolya.tar@gmail.com>
2021-08-25 18:39:54 +0300

    images css changed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './header.css';
 import Button from "../button";
+import logo from '../../images/logo.png';
 
 const Header = () => {
 
     return (
         <div className='header'>
-            <div className='logo'></div>
-            <img src='../images/logo.png' alt='logo' />
+            <div>
+                <img src={logo} alt='logo' className='logo'/>
+            </div>
             <Button variant='sing' label='Sing In'/>
         </div>
     )
--- src/components/postItem/index.js
+++ src/components/postItem/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './postItem.css';
-
+import postImg from '../../images/post-img.jpg'
 
 const PostItem = () => {
 
@@ -11,7 +11,9 @@ const PostItem = () => {
                 <p>Семейные ценности – это обычаи и традиции, которые передаются из поколения в поколение. Это – чувства, благодаря которым семья становится крепкой. Это – всё то, что люди переживают вместе внутри дома – радость и горе, благополучие или проблемы и трудности...</p>
                 <div><span>дата / автор</span></div>
             </div>
-            <div className='postItem-img'></div>
+            <div>
+                <img src={postImg} alt='post-img' className='post-img'/>
+            </div>
         </div>
     )
 }
