b511efae847e5305e82f73b9055e62def8b7a48b
onotole-ololoev <tolya.tar@gmail.com>
2021-09-05 10:53:27 +0300

    some pages fixed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Button from "./components/button";
 import Modal from "./components/modal";
 import Input from "./components/input";
 import Post from "./pages/post/post";
+import Error from "./pages/404/404";
 
 
 
@@ -39,6 +40,9 @@ function App(props) {
               </Route>
               <Route path="/post">
                   <Post />
+              </Route>
+              <Route path="/404">
+                  <Error />
               </Route>
               <Redirect to="/404" />
           </Switch>
